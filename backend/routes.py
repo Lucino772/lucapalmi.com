a8b6ce72c287ed73b0b8873a63dd7d7d3ed94cd5
Lucino772 <lucapalmi772@gmail.com>
2020-10-05 20:42:13 +0200

    Initial Flask Setup

---
+++ backend/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request, send_from_directory, current_app
+
+routes = Blueprint("routes",__name__)
+
+@routes.route("/")
+def index():
+    return render_template("index.html")
+
+
+@routes.route("/robots.txt")
+@routes.route("/sitemap.xml")
+def seo_files():
+    return send_from_directory(current_app.static_folder, request.path[1:])
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from backend.app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True,host='0.0.0.0')
