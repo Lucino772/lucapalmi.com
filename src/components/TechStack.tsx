226325810bb0fbb048644a09bc47bc01d0b25b6d
Lucino772 <lucapalmi772@gmail.com>
2023-05-20 09:48:23 +0200

    feat: updated TechStack component

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import * as technologies from "@/lib/technologies.json";
+import { Technologies } from "@/lib/types";
 
 type Props = {
-    icon: string,
-    label: string
+    name: keyof Technologies,
 };
 
-export default function TechStack({ icon, label }: Props) {
+export default function TechStack({ name }: Props) {
+    const { label, image, alt } = technologies[name];
+
     return (
         <div className="flex flex-row justify-center items-center gap-2 px-2 py-2 drop-shadow rounded-3xl" style={{ background: "#1d1d1d" }}>
             <Image 
-                src={icon}
-                alt="tech-stack-icon"
+                src={image}
+                alt={alt}
                 width={32}
                 height={32}
                 className="w-8 h-8 object-cover rounded-full"
---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+import * as technologies from "@/lib/technologies.json";
+
+type Technologies = typeof technologies;
+
+type ProjectMeta = {
+    title: string,
+    thumbnail: string,
+    description: string,
+    links: {
+        [key: string]: string
+    },
+    techs: (keyof Technologies)[]
+}
+
+export type { ProjectMeta, Technologies };
