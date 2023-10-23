18381426042a07cb7c84dc4a7d1e2bbf5aa4358b
Lucino772 <lucapalmi772@gmail.com>
2023-10-23 23:22:38 +0200

    fix: fixed escape `'`

--- src/components/AboutText.tsx
+++ src/components/AboutText.tsx
@@ -44,8 +44,8 @@ export default function AboutText() {
           exit="exit"
           variants={pVariants}
         >
-          My name is Luca Palmisano. I'm 20 years old and I'm currently studying
-          computer science at university.
+          {`My name is Luca Palmisano. I'm 20 years old and I'm currently studying
+          computer science at university.`}
         </motion.p>
         <motion.p
           className="relative mt-3 text-xl font-normal leading-6 md:pl-6"
@@ -55,10 +55,10 @@ export default function AboutText() {
           exit="exit"
           variants={pVariants}
         >
-          I've been coding for several years now. Through the years, what was
+          {`I've been coding for several years now. Through the years, what was
           once a hobby has became a passion. I have always been interested in
           discovering new things and this has helped me to expand my knowledge
-          of information technology.
+          of information technology.`}
         </motion.p>
         <motion.p
           className="relative mt-3 text-xl font-normal leading-6 md:pl-8"
@@ -68,9 +68,9 @@ export default function AboutText() {
           exit="exit"
           variants={pVariants}
         >
-          Besides that I am enjoying living a student's life, having fun with my
+          {`Besides that I am enjoying living a student's life, having fun with my
           friends, playing games, etc. Well, there you have it, this is me! This
-          is who I am!
+          is who I am!`}
         </motion.p>
       </div>
     </div>
