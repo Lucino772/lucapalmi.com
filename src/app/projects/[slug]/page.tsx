9623cce440c92df2da2ef588c33573aa807b0e9d
Lucino772 <lucapalmi772@gmail.com>
2023-10-23 22:07:14 +0200

    fix: every page now works properly

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,35 @@
+import * as fs from "fs/promises";
+import rehypeHighlight from "rehype-highlight";
+
+import NavBar from "@/components/NavBar";
+import { serialize } from "next-mdx-remote/serialize";
+import { getProject } from "@/lib/projects";
+
+import { ProjectMeta } from "@/lib/types";
+import { ProjectView } from "@/components/projects";
+
+async function getProjectInfo(slug: string) {
+  const project = await getProject(slug);
+  const source = await fs.readFile(project.filename, { encoding: "utf-8" });
+  const markdown = await serialize(source, {
+    parseFrontmatter: true,
+    mdxOptions: {
+      rehypePlugins: [rehypeHighlight],
+    },
+  });
+  return {
+    meta: markdown.frontmatter as ProjectMeta,
+    content: markdown,
+    slug: project.slug
+  };
+}
+
+export default async function Project({ params }: { params: { slug: string } }) {
+  const { meta, content } = await getProjectInfo(params.slug);
+  return (
+    <div style={{ paddingBottom: 100 }}>
+      <NavBar showLogo exitDelay={0.2} />
+      <ProjectView meta={meta} content={content} />
+    </div>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,47 @@
+import * as fs from "fs/promises";
+import { serialize } from "next-mdx-remote/serialize";
+
+import NavBar from "@/components/NavBar";
+import { getProjects } from "@/lib/projects";
+import { ProjectMeta } from "@/lib/types";
+import { ProjectList } from "@/components/projects";
+import rehypeHighlight from "rehype-highlight";
+
+type Props = {
+  projects: {
+    meta: ProjectMeta;
+    slug: string;
+  }[];
+};
+
+async function getProjectsInfo() {
+  const projects = await Promise.all(
+    (
+      await getProjects()
+    ).map(async (project) => {
+      const source = await fs.readFile(project.filename, { encoding: "utf-8" });
+      const markdown = await serialize(source, {
+        parseFrontmatter: true,
+        mdxOptions: {
+          rehypePlugins: [rehypeHighlight],
+        },
+      });
+      return {
+        meta: markdown.frontmatter as ProjectMeta,
+        slug: project.slug,
+      };
+    })
+  );
+  return projects;
+}
+
+export default async function Projects(props: Props) {
+  const projects = await getProjectsInfo();
+
+  return (
+    <div style={{ paddingBottom: 100 }}>
+      <NavBar showLogo exitDelay={0.2} />
+      <ProjectList projects={projects} />
+    </div>
+  );
+}
