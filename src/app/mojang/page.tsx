4e7a8f8da3e08fb69af388427f8cd8601dee8520
Lucino772 <lucapalmi772@gmail.com>
2024-09-16 22:43:31 +0200

    fix: fixed issue with light artifacts

--- src/app/mojang/page.tsx
+++ src/app/mojang/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
         <div className="h-full">
             <Canvas
                 flat
-                gl={{ antialias: false }}
+                gl={{ antialias: true }}
                 shadows
                 camera={{ far: 5000 }}
             >
